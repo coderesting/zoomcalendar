c5a9ad6e05fd2496ad488177cdb1e6546146088d
Yannick Brandt <yannick-brandt@outlook.de>
2021-06-03 10:10:41 +0200

    removed wrong directory

--- zoomcalendar/sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('fetch', async (event) => {
-	if (event.request.mode === 'navigate') {
-		return event.respondWith(
-			fetch(event.request).catch(
-				() => new Response('Check your connection')
-			)
-		);
-	}
-});
